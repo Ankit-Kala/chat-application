a80c71e0a57a79cac12506bdb4226474d808adf6
root <ankitkala.webethics@gmail.com>
2024-03-15 11:53:04 +0530

    User Seeders Implemented

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // User::factory(5)->create();
+
+        User::create([
+            'name'  => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin@123'),
+        ]);
+
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => Hash::make('user@1234'),
+        ]);
+
+        User::create([
+            'name' => 'Sam Curran',
+            'email' => 'sam@example.com',
+            'password' => Hash::make('user@1234'),
+        ]);
+
+        User::create([
+            'name' => 'Jason Roy',
+            'email' => 'jason@example.com',
+            'password' => Hash::make('user@1234'),
+        ]);
+
+        User::create([
+            'name' => 'Alex Hails',
+            'email' => 'alex@example.com',
+            'password' => Hash::make('user@1234'),
+        ]);
+    }
+}
